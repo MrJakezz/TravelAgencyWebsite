a186e6073af4325a85c817efb87628b78c5b222d
tugcan.aygul <tugcan.aygul@meritnet.com>
2023-10-19 20:03:40 +0300

    Not Finished

--- TraversalCoreProject/Areas/Admin/Controllers/PdfReportController.cs
+++ TraversalCoreProject/Areas/Admin/Controllers/PdfReportController.cs
@@ -34,28 +34,39 @@ namespace TraversalCoreProject.Areas.Admin.Controllers
 
         public IActionResult StaticCustomerReport()
         {
-            string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/PDFReports/" + "pdf1.pdf");
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/PDFReports/" + "PdfTable.pdf");
 
             var stream = new FileStream(path, FileMode.Create);
 
             Document document = new Document(PageSize.A4);
             PdfWriter.GetInstance(document, stream);
 
+            //Claim Arial font.   
+            string Arial_TFF = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Fonts), "Arial.ttf");
+
+            //Create an instance for BaseFont.
+            BaseFont baseFont = BaseFont.CreateFont(Arial_TFF, BaseFont.IDENTITY_H, true);
+
+            //Create an instance for font.
+            Font font = new Font(baseFont, 12, Font.NORMAL);
+
             document.Open();
 
             PdfPTable pdfPTable = new PdfPTable(3);
 
-            pdfPTable.AddCell("Misafir Adı");
-            pdfPTable.AddCell("Misafir Soyadı");
-            pdfPTable.AddCell("Misafir TC");
+            pdfPTable.AddCell(new Phrase("Misafir Adı", font));
+            pdfPTable.AddCell(new Phrase("Misafir Soyadı", font));
+            pdfPTable.AddCell(new Phrase("Misafir TC", font));
 
-            pdfPTable.AddCell("Tuğcan");
-            pdfPTable.AddCell("Aygül");
-            pdfPTable.AddCell("11111111110");
+            pdfPTable.AddCell(new Phrase("Tuğcan", font));
+            pdfPTable.AddCell(new Phrase("Aygül", font));
+            pdfPTable.AddCell(new Phrase("11111111110", font));
+
+            document.Add(pdfPTable);
 
             document.Close();
 
-            return File("/PDFReports/pdf1.pdf", "application/pdf", "pdf1.pdf");
+            return File("/PDFReports/PdfTable.pdf", "application/pdf", "PdfTable.pdf");
         }
     }
 }
